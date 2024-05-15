25b6caeecaa025a7436a705cfb8702d972607204
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-05-14 23:51:45 -0300

    Add beecrowd problem 1984

---
+++ beecrowd/1-beginner/1984.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    long long int n;
+    cin >> n;
+    while (n > 0) {
+        cout << n%10;
+        n /= 10;
+    }
+    cout << endl;
+    return 0;
+}
