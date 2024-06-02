ca7b2353bcacf5de03c0179045fed996941b4195
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-06-02 15:32:30 -0300

    Add beecrowd problem 2483

---
+++ beecrowd/1-beginner/2483.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int i;
+    cin >> i;
+    cout << "Feliz nat";
+    while (i--) {
+        cout << "a";
+    }
+    cout << "l!" << endl;
+    return 0;
+}
