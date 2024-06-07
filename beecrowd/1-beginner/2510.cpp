e4e7a77a4ee3e1d71a5bb027a053972f96319cde
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-06-06 22:34:09 -0300

    Add beecrowd problem 2510

---
+++ beecrowd/1-beginner/2510.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string villain;
+    int n;
+    cin >> n;
+    while (n--) {
+        getline(cin, villain);
+        cout << "Y" << endl;
+    }
+    return 0;
+}
