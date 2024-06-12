dee3c308ce938ab8ba392ffe6baaad9d8a6d9642
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-06-12 18:42:09 -0300

    Add beecrowd problem 2581

---
+++ beecrowd/1-beginner/2581.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    string question;
+    cin >> n;
+    while (n--) {
+        cin.ignore();
+        getline(cin, question);
+        cout << "I am Toorg!" << endl;
+    }
+    return 0;
+}
