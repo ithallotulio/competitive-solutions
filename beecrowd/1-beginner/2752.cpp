a96d3997b66989f02fa58950c2f5aa46ce7bd8b9
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-07-12 15:40:59 -0300

    Add beecrowd problem 2752

---
+++ beecrowd/1-beginner/2752.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string phrase = "AMO FAZER EXERCICIO NO URI";
+    printf("<%s>\n", phrase.c_str());
+    printf("<%30s>\n", phrase.c_str());
+    printf("<%.20s>\n", phrase.c_str());
+    printf("<%-20s>\n", phrase.c_str());
+    printf("<%-30s>\n", phrase.c_str());
+    printf("<%.30s>\n", phrase.c_str());
+    printf("<%30.20s>\n", phrase.c_str());
+    printf("<%-30.20s>\n", phrase.c_str());
+    return 0;
+}
