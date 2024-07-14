9fc25c5fa626dcb36d53d16ca007a8d36c1248ef
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-07-14 10:32:37 -0300

    Add beecrowd problem 2766

---
+++ beecrowd/1-beginner/2766.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string names[10];
+    for (int i=0; i < 10; i++) {
+        cin >> names[i];
+    }
+    cout << names[2] << endl;
+    cout << names[6] << endl;
+    cout << names[8] << endl;
+    return 0;
+}
