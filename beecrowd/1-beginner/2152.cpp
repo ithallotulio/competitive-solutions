eb25519a904054f2d8323be847130d3b91d7692e
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-07-17 18:00:28 -0300

    Refactor beecrowd problem 2152

--- beecrowd/1-beginner/2152.cpp
+++ beecrowd/1-beginner/2152.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 int main() {
-    string hoursFormatted, minutesFormatted;
+    string doorState;
     int n, hours, minutes, isDoorOpen;
     cin >> n;
     while (n--) {
         cin >> hours >> minutes >> isDoorOpen;
-        hoursFormatted = (hours < 10) ? "0" +  to_string(hours) : to_string(hours);
-        minutesFormatted = (minutes < 10) ? "0" + to_string(minutes) : to_string(minutes);
-        cout << hoursFormatted << ":" << minutesFormatted << " - " << "A porta ";
-        if (isDoorOpen) cout << "abriu!" << endl;
-        else cout << "fechou!" << endl;
+        if (isDoorOpen) {
+            doorState = "abriu";
+        } else {
+            doorState = "fechou";
+        }
+        printf("%02d:%02d - A porta %s!\n", hours, minutes, doorState.c_str());
     }
     return 0;
 }
