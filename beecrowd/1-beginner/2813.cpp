6822b96fec118d29bab2742d08e9611b04aa5e56
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-08-05 23:42:02 -0300

    [unfinished] Add beecrowd problem 2813

---
+++ beecrowd/1-beginner/2813.cpp
@@ -0,0 +1,54 @@
+// NEEDS FINISH
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, i;
+    cin >> n;
+    int storageHome = 0;
+    int storageOffice = 0;
+    int buyOffice = 0;
+    int buyHome = 0;
+    bool carryingUmbrella = false;
+    for (i = 0; i < n*2; i++) {
+        string weather;
+        cin >> weather;
+        bool leavingHome = (i % 2) == 0 ? true : false;
+        bool isRaining = weather == "chuva" ? true : false;
+
+        if (leavingHome) {
+            if (isRaining) {
+                if (!carryingUmbrella) {
+                    carryingUmbrella = true;
+                    if (storageHome > 0) storageHome--;
+                    else buyHome++;
+                }
+            } else {
+                if (carryingUmbrella) storageHome++;
+                carryingUmbrella = false;
+            }
+        } else { // leavingOffice
+            if (isRaining) {
+                if (!carryingUmbrella) {
+                    carryingUmbrella = true;
+                    if (storageOffice > 0) storageOffice--;
+                    else buyOffice++;
+                }
+            } else {
+                if (carryingUmbrella) storageOffice++;
+                carryingUmbrella = false;
+            }
+        }
+    }
+    if (carryingUmbrella) {
+        if ((i-1) % 2 == 0) {
+            storageOffice++;
+        } else {
+            storageHome++;
+        }
+    }
+    cout << storageHome << " " << storageOffice << endl;
+    return 0;
+}
